ffaec42d17da0640e9fce989ea97f039cd3bc535
cooldeep004 <kuldeepsinghbvpcoep@gmail.com>
2020-09-30 02:23:33 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,55 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       users: [],
       isLoading: false,
-      isError: false,
+      isError: false,  
+      isShortlist: false,
       value: '',
-      shortlist: []
+      shortlist: [],
+      statearray: [{"City":"","District":"","State":""}],  // to add new city
+      City: '',
+      State:'',
+      District:''
+       
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangea = this.handleChangea.bind(this);
+    this.handleChangeb = this.handleChangeb.bind(this);
+    this.handleChangec = this.handleChangec.bind(this);
+    this.handleSubmitt = this.handleSubmitt.bind(this);
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
+  handleChangea(event) {
+    this.setState({ City: event.target.value });
+  }
+  handleChangeb(event) {
+    this.setState({ District: event.target.value });
+  }
+  handleChangec(event) {
+    this.setState({ State: event.target.value });
+  }
+
 
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
-  deleteEmployee(id) {
-    console.log(id);
-    fetch('https://api.jsonbin.io/b/5f5c76a5302a837e9564b5ca' + id,
-      {
-        method: 'DELETE',
-        credentials: 'same-origin'
-      })
-      .then(
-        res => {
-          var employee = [...this.state.users];
-          console.log(employee)
-          var idx = employee.findIndex(item => item.City === id);
-          employee.splice(idx, 1);
-          this.setState({ users: employee })
-        }
-      )
-      .catch(err => console.error(err))
+  handleSubmitt(event) {
+    alert('A city added successfully ');
+   console.log(this.state.statearray)
+    event.preventDefault();
   }
 
-  additems(id) {
-    this.state.shortlist.push(id);
+  deleteRow(id) {                          //for deleting the city
+    var array = [...this.state.shortlist]; // make a separate copy of the array
+    var index = array.indexOf(id)
+    if (index !== -1) {
+      array.splice(index, 1);
+      this.setState({ shortlist: array });
+    }
+
+    console.log(this.state.shortlist)
+    console.log(index);
+    var array1 = [...this.state.users]; // make a separate copy of the array
+    var index1 = array1.indexOf(id)
+    if (index1 !== -1) {
+      array1.splice(index1, 1);
+      this.setState({ users: array1 });
+    }
+    alert('Are you sure you want to delete ' + id.City);
+    
+  }
+ 
+  deleteShortlisted(id) {                  //for deleting from shortlisted
+    var array = [...this.state.shortlist]; // make a separate copy of the array
+    var index = array.indexOf(id)
+    if (index !== -1) {
+      array.splice(index, 1);
+      this.setState({ shortlist: array });
+    }
+  }
+  additems = (id) => {                     //add city to shortlist
+    var row = [...this.state.shortlist];
+    var idx = row.findIndex(item => item === id);
+    if (idx === -1)
+      this.setState({ shortlist: [...this.state.shortlist, id] })
     console.log(this.state.shortlist);
   }
-
-
-  async componentDidMount() {
+ 
+  async componentDidMount() {         //fetchind data from given api
     this.setState({ isLoading: true })
     const response = await fetch("https://api.jsonbin.io/b/5f5c76a5302a837e9564b5ca")
     if (response.ok) {
@@ -63,12 +100,44 @@ class App extends Component {
     }
 
   }
-  renderTableHeader = () => {
+  renderTableHeader = () => {           //rendering table header
     return Object.keys(this.state.users[0]).map(attr => <th key={attr}>{attr.toUpperCase()}</th>)
-  }
-  renderTableRows = () => {
+  } 
+  renderTableRows = () => {             //render the table data
+    console.log(this.state.data)
     var sr = 1;
-    return this.state.users.filter(user => user.State.includes(this.state.value) || user.District.includes(this.state.value)).map(userf => {
+    return this.state.users.filter(user => user.State.toLowerCase().includes(this.state.value.toLowerCase()) ||
+      user.District.toLowerCase().includes(this.state.value.toLowerCase()) ||
+      user.City.toLowerCase().includes(this.state.value.toLowerCase())).map(userf => {
+
+        return (
+          <tr>
+            <td>{sr++}</td>
+            <td>{userf.City}</td>
+            <td>{userf.State}</td>
+            <td>{userf.District}</td>
+            <td className="bt"><button onClick={() => this.deleteRow(userf)} style={{ color: "red" }} >delete</button></td>
+            <td className="bt"><button onClick={() => this.additems(userf)} style={{ color: "green" }}>shortlist</button></td>
+          </tr>
+        )
+      })
+  }
+  shortlistdata = () => { //to show shortlisted data
+    this.setState({ isError: true })
+  }
+
+  shortlistdataAll = () => { //to show all data
+
+    this.setState({ isError: false })
+  }
+
+  renderTableRowsShortlist = () => { //shortlisted data
+    var sr = 1;
+    if (this.state.shortlist.length < 1)
+      return <div>nothing shortlisted</div>
+    return this.state.shortlist.filter(user => user.State.toLowerCase().includes(this.state.value.toLowerCase()) ||
+    user.District.toLowerCase().includes(this.state.value.toLowerCase()) ||
+    user.City.toLowerCase().includes(this.state.value.toLowerCase())).map(userf => {
 
       return (
         <tr>
@@ -76,28 +145,13 @@ class App extends Component {
           <td>{userf.City}</td>
           <td>{userf.State}</td>
           <td>{userf.District}</td>
-          <td><button onClick={() => this.deleteEmployee(userf.City)}>delete</button></td>
-          <td><button onClick={() => this.additems(userf.City)}>shortlist</button></td>
+          <td className="bt"><button onClick={() => this.deleteShortlisted(userf)} style={{ color: "red" }}>remove shortlist</button></td>
         </tr>
       )
     })
   }
-  shortlistdata = () => {
-  var sr=1;
-return this.state.shortlist.map(userf =>{
-  
-  return(
-    <tr>
-      <td>{sr++}</td>
-      <td>{userf.City}</td>
-      <td>{userf.State}</td>
-      <td>{userf.District}</td>
-     
-    </tr>
-  )
-})
 
-  }
+
 
   render() {
 
@@ -105,25 +159,69 @@ return this.state.shortlist.map(userf =>{
     if (isLoading) {
       return <div>Loading....</div>
     }
-    if (isError) {
-      return <div>Error</div>
-    }
-    return users.length > 0
-      ? (
-        <div>
+    if (isError) {    // to show shortlisted data
+      return <div style={{ background: "skyblue" }}>
+        <div className="header">
           <form onSubmit={this.handleSubmit}>
             <label>
               Name:
-          <input type="text" placeholder="first letter should be capital" value={this.state.value} onChange={this.handleChange} />
+      <input type="text" placeholder="Enter city,district or state" value={this.state.value} onChange={this.handleChange} />
             </label>
             <input type="submit" value="Submit" />
 
           </form>
-          <button onClick={this.shortlistdata()}>shortlist</button>
+          <button onClick={() => this.shortlistdata()}>shortlisted</button>
+          <button onClick={() => this.shortlistdataAll()}>All</button>
+
+        </div>
+        <div>
+
+        </div>
+        <table border="4">
+          <thead>
+            <tr>
+              <td>SR</td> {this.renderTableHeader()}
+              <td>ACTION</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTableRowsShortlist()}
+          </tbody>
+        </table>
+      </div>
+    }
+
+    return users.length > 0  // to show all data
+      ? (
+        <div style={{ background: "skyblue" }}>
+          <div className="header">
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Search:
+          <input type="text" placeholder="Enter city,district or state" value={this.state.value} onChange={this.handleChange} />
+              </label>
+
+            </form>
+            <form onSubmit={this.handleSubmitt}>
+              <label>
+               
+          <input type="text" placeholder="Enter city" value={this.state.City} onChange={this.handleChangea} />
+
+          <input type="text" placeholder="Enter district" value={this.state.District} onChange={this.handleChangeb} />
+           
+          <input type="text" placeholder="Enter state" value={this.state.State} onChange={this.handleChangec} />
+              </label>
+              <input type="submit" value="Add" />
+
+            </form>
+            <button onClick={() => this.shortlistdata()}>shortlisted</button>
+            <button onClick={() => this.shortlistdataAll()}>All</button>
+          </div>
           <table border="4">
             <thead>
-              <tr>
-               <td>sr</td> {this.renderTableHeader()}
+              <tr style={{}}>
+                <td>SR</td> {this.renderTableHeader()}
+                <td>ACTION</td>
               </tr>
             </thead>
             <tbody>
